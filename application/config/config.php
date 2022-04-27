7e4948917e9f46919518b2864e3b2793cfde430a
Komal-19207118 <komal.alfarabi18@gmail.com>
2022-04-27 12:23:02 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/WP2_Komal';
+$config['base_url'] = 'http://localhost/WP2-Komal';
 
 /*
 |--------------------------------------------------------------------------
